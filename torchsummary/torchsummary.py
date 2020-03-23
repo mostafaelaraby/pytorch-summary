636b22484806b82990487329afd9c3ba64a2acdd
Mostafa ElAraby <4073898+mostafaelaraby@users.noreply.github.com>
2020-03-22 20:48:46 -0400

    added support to print hyperparams

--- torchsummary/torchsummary.py
+++ torchsummary/torchsummary.py
@@ -6,19 +6,36 @@ from collections import OrderedDict
 import numpy as np
 
 
-def summary(model, input_size, batch_size=-1, device=torch.device('cuda:0'), dtypes=None):
+def summary(
+    model,
+    input_size,
+    batch_size=-1,
+    device=torch.device("cuda:0"),
+    dtypes=None,
+    with_hyper_params=False,
+):
+    if not (torch.cuda.is_available()):
+        device = torch.device("cpu")
     result, params_info = summary_string(
-        model, input_size, batch_size, device, dtypes)
+        model, input_size, batch_size, device, dtypes, with_hyper_params
+    )
     print(result)
 
     return params_info
 
 
-def summary_string(model, input_size, batch_size=-1, device=torch.device('cuda:0'), dtypes=None):
+def summary_string(
+    model,
+    input_size,
+    batch_size=-1,
+    device=torch.device("cuda:0"),
+    dtypes=None,
+    with_hyper_params=False,
+):
     if dtypes == None:
-        dtypes = [torch.FloatTensor]*len(input_size)
+        dtypes = [torch.FloatTensor] * len(input_size)
 
-    summary_str = ''
+    summary_str = ""
 
     def register_hook(module):
         def hook(module, input, output):
@@ -31,7 +48,7 @@ def summary_string(model, input_size, batch_size=-1, device=torch.device('cuda:0
             summary[m_key]["input_shape"][0] = batch_size
             if isinstance(output, (list, tuple)):
                 summary[m_key]["output_shape"] = [
-                    [-1] + list(o.size())[1:] for o in output
+                    [-1] + list(o.size())[1:] for o in output if o is not None
                 ]
             else:
                 summary[m_key]["output_shape"] = list(output.size())
@@ -44,10 +61,17 @@ def summary_string(model, input_size, batch_size=-1, device=torch.device('cuda:0
             if hasattr(module, "bias") and hasattr(module.bias, "size"):
                 params += torch.prod(torch.LongTensor(list(module.bias.size())))
             summary[m_key]["nb_params"] = params
+            summary[m_key]["hyper_params"] = {}
+            if with_hyper_params:
+                primitive = (int, str, bool, float, tuple)
+                for attr, value in module.__dict__.items():
+                    if value is None or attr == "training":
+                        continue
+                    if isinstance(value, primitive):
+                        summary[m_key]["hyper_params"][attr] = str(value)
 
-        if (
-            not isinstance(module, nn.Sequential)
-            and not isinstance(module, nn.ModuleList)
+        if not isinstance(module, nn.Sequential) and not isinstance(
+            module, nn.ModuleList
         ):
             hooks.append(module.register_forward_hook(hook))
 
@@ -56,8 +80,10 @@ def summary_string(model, input_size, batch_size=-1, device=torch.device('cuda:0
         input_size = [input_size]
 
     # batch_size of 2 for batchnorm
-    x = [torch.rand(2, *in_size).type(dtype).to(device=device)
-         for in_size, dtype in zip(input_size, dtypes)]
+    x = [
+        torch.rand(2, *in_size).type(dtype).to(device=device)
+        for in_size, dtype in zip(input_size, dtypes)
+    ]
 
     # create properties
     summary = OrderedDict()
@@ -73,12 +99,19 @@ def summary_string(model, input_size, batch_size=-1, device=torch.device('cuda:0
     # remove these hooks
     for h in hooks:
         h.remove()
-
-    summary_str += "----------------------------------------------------------------" + "\n"
+    if with_hyper_params:
+        table_width = 100
+    else:
+        table_width = 64
+    print("-" * table_width)
     line_new = "{:>20}  {:>25} {:>15}".format(
-        "Layer (type)", "Output Shape", "Param #")
-    summary_str += line_new + "\n"
-    summary_str += "================================================================" + "\n"
+        "Layer (type)", "Output Shape", "Param #"
+    )
+    if with_hyper_params:
+        line_new = "{:>64} {:>35}".format(line_new, "Layer params")
+
+    print(line_new)
+    print("=" * table_width)
     total_params = 0
     total_output = 0
     trainable_params = 0
@@ -95,26 +128,41 @@ def summary_string(model, input_size, batch_size=-1, device=torch.device('cuda:0
         if "trainable" in summary[layer]:
             if summary[layer]["trainable"] == True:
                 trainable_params += summary[layer]["nb_params"]
-        summary_str += line_new + "\n"
+        if len(summary[layer]["hyper_params"]) > 0:
+            for hyper_param, hyper_param_value in summary[layer][
+                "hyper_params"
+            ].items():
+                line_new = "{:>64} {:>15}{:>20}".format(
+                    line_new, hyper_param, hyper_param_value
+                )
+                print(line_new)
+                summary_str += line_new + "\n"
+                line_new = ""
+        else:
+            print(line_new)
+            summary_str += line_new + "\n"
 
     # assume 4 bytes/number (float on cuda).
-    total_input_size = abs(np.prod(sum(input_size, ()))
-                           * batch_size * 4. / (1024 ** 2.))
-    total_output_size = abs(2. * total_output * 4. /
-                            (1024 ** 2.))  # x2 for gradients
-    total_params_size = abs(total_params * 4. / (1024 ** 2.))
+    total_input_size = abs(
+        np.prod(sum(input_size, ())) * batch_size * 4.0 / (1024 ** 2.0)
+    )
+    total_output_size = abs(
+        2.0 * total_output * 4.0 / (1024 ** 2.0)
+    )  # x2 for gradients
+    total_params_size = abs(total_params * 4.0 / (1024 ** 2.0))
     total_size = total_params_size + total_output_size + total_input_size
 
-    summary_str += "================================================================" + "\n"
+    summary_str += "=" * table_width + "\n"
     summary_str += "Total params: {0:,}".format(total_params) + "\n"
     summary_str += "Trainable params: {0:,}".format(trainable_params) + "\n"
-    summary_str += "Non-trainable params: {0:,}".format(total_params -
-                                                        trainable_params) + "\n"
-    summary_str += "----------------------------------------------------------------" + "\n"
+    summary_str += (
+        "Non-trainable params: {0:,}".format(total_params - trainable_params) + "\n"
+    )
+    summary_str += "-" * table_width + "\n"
     summary_str += "Input size (MB): %0.2f" % total_input_size + "\n"
     summary_str += "Forward/backward pass size (MB): %0.2f" % total_output_size + "\n"
     summary_str += "Params size (MB): %0.2f" % total_params_size + "\n"
     summary_str += "Estimated Total Size (MB): %0.2f" % total_size + "\n"
-    summary_str += "----------------------------------------------------------------" + "\n"
+    summary_str += "-" * table_width + "\n"
     # return summary
     return summary_str, (total_params, trainable_params)
